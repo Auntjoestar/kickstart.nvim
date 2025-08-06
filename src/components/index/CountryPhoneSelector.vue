496c8b7898f385029ad110f99ead1e4efad7c8fa
auntjoestar <88282065+Auntjoestar@users.noreply.github.com>
2025-08-06 00:49:44 -0600

    Add form sending service and auth

--- src/components/index/CountryPhoneSelector.vue
+++ src/components/index/CountryPhoneSelector.vue
@@ -1,13 +1,31 @@
 <script setup>
-import IntlTelInput from "intl-tel-input/vue";
-import "intl-tel-input/styles";
+import { ref, onMounted } from 'vue';
+import IntlTelInput from 'intl-tel-input/vue';
+
+const phoneInputRef = ref();
+const fullNumber = ref('');
+
+function updateDialCode() {
+  const instance = phoneInputRef.value?.instance;
+  if (!instance) return;
+
+  const dialCode = instance.getSelectedCountryData().dialCode;
+  const hiddenInput = document.querySelector('input[name="extension"]');
+  if (hiddenInput) {
+    hiddenInput.value = `+${dialCode}`;
+  }
+}
+
+function handleCodeUpdate() {
+	updateDialCode()
+}
 </script>
 
 <template>
   <IntlTelInput
-    @changeNumber="number = $event"
-    @changeValidity="isValid = $event"
-    @changeErrorCode="errorCode = $event"
+    ref="phoneInputRef"
+    @changeCountry="handleCodeUpdate"
+    :inputProps="{ required: true, id: 'phone', name: 'phone' }"
     :options="{
       initialCountry: 'ni',
       loadUtils: () => import('intl-tel-input/build/js/utils.js'),
